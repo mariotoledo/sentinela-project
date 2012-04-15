ef48c67cd01b7803228254a99bea5a08a9b8acfb
Mario Toledo <mariotoledo12@gmail.com>
2012-04-15 01:32:29 -0300

    Adicionando o botao de conexao e a permissao para bluetooth

--- src/org/sentinela/SentinelaActivity.java
+++ src/org/sentinela/SentinelaActivity.java
@@ -1,13 +1,28 @@
 package org.sentinela;
 
 import android.app.Activity;
+import android.widget.Button;
 import android.os.Bundle;
+import android.view.View.OnClickListener;
+import android.view.View;
 
 public class SentinelaActivity extends Activity {
-    /** Called when the activity is first created. */
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        Button connectButton = (Button) findViewById(R.id.connectButton);
+        
+        connectButton.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+            	connectBluetooth();
+                //v.setVisibility(View.GONE);
+            }
+        });
+    }
+    
+    private void connectBluetooth(){
+    	//conect bluetooh
     }
 }

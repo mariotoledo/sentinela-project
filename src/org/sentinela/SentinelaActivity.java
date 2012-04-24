21b0909119b6750ae4bc96b36a1c02da330e52b1
Mario Toledo <mariotoledo12@gmail.com>
2012-04-24 12:48:28 -0300

    Encontrando os dispositivos bluetooth

--- src/org/sentinela/SentinelaActivity.java
+++ src/org/sentinela/SentinelaActivity.java
@@ -1,13 +1,19 @@
 package org.sentinela;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.bluetooth.*;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.os.Bundle;
 import android.view.View.OnClickListener;
 import android.view.View;
@@ -18,16 +24,22 @@ public class SentinelaActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.device_list);
         Button connectButton = (Button) findViewById(R.id.connectButton);
-        
-        mArrayAdapter = new ArrayAdapter<String>(this, 0);
         
         connectButton.setOnClickListener(new OnClickListener() {
             public void onClick(View v) {
             	connectBluetooth();
             }
         });
+        
+        
+        ArrayList<String> aus = new ArrayList<String>();
+        mArrayAdapter = new ArrayAdapter<String>(this,R.layout.device_name);
+        
+        ListView newDevicesListView = (ListView) findViewById(R.id.new_devices);
+        newDevicesListView.setAdapter(mArrayAdapter);
+        newDevicesListView.setOnItemClickListener(mDeviceClickListener);
         
         IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
         this.registerReceiver(ultimateReceiver, filter);
@@ -72,6 +84,12 @@ public class SentinelaActivity extends Activity {
                 mArrayAdapter.add(device.getName() + "\n" + device.getAddress());
                 System.out.println("" + device.getName() + "\n" + device.getAddress());
             }
+        }
+    };
+    
+    private OnItemClickListener mDeviceClickListener = new OnItemClickListener() {
+        public void onItemClick(AdapterView<?> av, View v, int arg2, long arg3) {
+            System.out.println("clicado");
         }
     };
     
